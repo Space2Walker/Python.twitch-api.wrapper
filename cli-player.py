b468e8a01fe6b3d84fa90c516d73ab376a4148ef
Space2Walker <21970520+Space2Walker@users.noreply.github.com>
2020-05-02 22:35:56 +0200

    added fire_notifactions as stupid async fix  + copyright

--- cli-player.py
+++ cli-player.py
@@ -1,6 +1,17 @@
-#!/usr/bin/env python3
-# author: Space2Walker
-# 2019-09-12
+#      Copyright (C) 2020  Space2Walker
+#
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import argparse
 from subprocess import Popen
@@ -14,78 +25,77 @@ site = "https://twitch.tv/"
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--play', help='Plays the Stream of a given user', type=str)
-    parser.add_argument('-v', '--vod', help='Shows the latest VOD`s of a given user', type=str)
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-p', '--play', help='Plays the Stream of a given user',
+						type=str)
+	parser.add_argument('-v', '--vod',
+						help='Shows the latest VOD`s of a given user', type=str)
+	args = parser.parse_args()
 
-    if args.play:
-        url = site + args.play
-        play(url)
+	if args.play:
+		url = site + args.play
+		play(url)
 
-    if args.vod:
-        vod(args.vod)
+	if args.vod:
+		vod(args.vod)
 
-    else:
-        stream()
+	else:
+		stream()
 
 
 # display options and get choice
 def display(index, typ='stream'):
-    init(autoreset=True)
-    user_input = None
+	init(autoreset=True)
+	user_input = None
 
-    for n, val in enumerate(index, start=1):
-        print(str(n) + ".")
-        if typ == 'stream':
-            print(Fore.GREEN + val.name)
-            print(val.title)
-        if typ == 'vod':
-            print(Fore.GREEN + val.title)
-            print(val.duration)
+	for n, val in enumerate(index, start=1):
+		print(str(n) + ".")
+		if typ == 'stream':
+			print(Fore.GREEN + val.name)
+			print(val.title)
+		if typ == 'vod':
+			print(Fore.GREEN + val.title)
+			print(val.duration)
 
-    try:
-        user_input = int(input("Number ?")) - 1
-    except KeyboardInterrupt:
-        print('\n')
-        exit()
+	try:
+		user_input = int(input("Number ?")) - 1
+	except KeyboardInterrupt:
+		print('\n')
+		exit()
 
-    return user_input
+	return user_input
 
 
-# play with streamlink
 def play(url):
-    command = ['streamlink', url, 'best', '-Q', '-p', 'cvlc']
-    Popen(command)
+	# play with streamlink
+	command = ['streamlink', url, 'best', '-Q', '-p', 'cvlc']
+	Popen(command)
 
 
-######################################
-#           Main Functions           #
-######################################
 def vod(user_name):
-    # get data
-    user_id = twitch.Streamer(user_name).user_id
-    index = twitch.search('VIDEOS', user_id=user_id, first=10)
+	# get data
+	user_id = twitch.Streamer(user_name).user_id
+	index = twitch.search('VIDEOS', user_id=user_id, first=10)
 
-    # display options and get choice
-    user_input = display(index, typ='vod')
+	# display options and get choice
+	user_input = display(index, typ='vod')
 
-    # play
-    url = str(index[user_input].url)
-    play(url)
+	# play
+	url = str(index[user_input].url)
+	play(url)
 
 
 def stream():
-    # get data
-    index = twitch.search('STREAMS', user_login=abos)
+	# get data
+	index = twitch.search('STREAMS', user_login=abos)
 
-    # display options and get choice
-    user_input = display(index, typ='stream')
+	# display options and get choice
+	user_input = display(index)
 
-    # play
-    url = site + str(index[user_input].name)
-    play(url)
+	# play
+	url = site + str(index[user_input].name)
+	play(url)
 
 
 if __name__ == '__main__':
-    main()
+	main()
--- notify.py
+++ notify.py
@@ -1,110 +1,54 @@
 #!/usr/bin/env python3
 # author: Space2Walker
 # 2019-09-12
+import subprocess
 import time
-
-import gi
+from os import path as opath
+from sys import path as spath
 
 import twitch
-
-gi.require_version('Notify', '0.7')
-gi.require_version('Gtk', '3.0')
-
 from abos import abos
-from subprocess import Popen
-from gi.repository import Notify
-from gi.repository import Gtk
-
-
-# play with streamlink
-def play_this(obj, action, url):
-    if action == 'dismiss':
-        Gtk.main_quit()
-
-    if action == 'play':
-        command = ['streamlink', url, 'best', '-Q']
-        Popen(command)
-        Gtk.main_quit()
 
 
 # notify helper
 def show_notification(name, title, url):
-    Notify.init("Twitch")
-    notification = Notify.Notification.new(name, title, "/home/lord/Documents/Twitch/twitch.png")
-    notification.add_action(
-        "play",
-        "Play",
-        play_this,
-        url  # Arguments
-    )
-    notification.add_action(
-        "dismiss",
-        "Dismiss",
-        play_this,
-        url  # Arguments
-    )
-    notification.show()
-    # todo find a solution to the event based notification callback
-    Gtk.main()
+	file = opath.join(spath[0], "fire_notification.py")
+	command = ['python', file, '-n', name, '-t', title, '-u', url]
+	subprocess.DETACHED_PROCESS = 1
+	subprocess.Popen(command)
 
 
 # run forever
 def main():
-    last_index = {}
-    first_round = True
-    # val setting
+	last_index = {}
+	first_round = True
 
-    # get data
-    index = twitch.search('STREAMS', dicta=True, user_login=abos)
+	# get data
+	index = twitch.search('STREAMS', dicta=True, user_login=abos)
 
-    if first_round:
-        for user_id, stream in index.items():
-            show_notification(stream.name, stream.title, stream.url)
+	if first_round:
+		for user_id, stream in index.items():
+			show_notification(stream.name, stream.title, stream.url)
 
-    # iterate over data an compare to last data
-    if not first_round:
-        for user_id, stream in index.items():
-            last = None
-            # there is now last wit that id so it must be new
-            try:
-                last = last_index[user_id]
-            except KeyError:
-                show_notification(stream.name, stream.title, stream.url)
-            else:
-                if (last.title != stream.title) or (last.game_id != stream.game_id):
-                    show_notification(stream.name, stream.title, stream.url)
+	# iterate over data an compare to last data
+	if not first_round:
+		for user_id, stream in index.items():
+			last = None
+			# there is now last wit that id so it must be new
+			try:
+				last = last_index[user_id]
+			except KeyError:
+				show_notification(stream.name, stream.title, stream.url)
+			else:
+				if (last.title != stream.title) or (
+					last.game_id != stream.game_id):
+					show_notification(stream.name, stream.title, stream.url)
 
-    # copy last data and set 2nd run flag then wait
-    last_index = index
-    first_round = False
-    time.sleep(300)
+	# copy last data and set 2nd run flag then wait
+	last_index = index
+	first_round = False
+	time.sleep(300)
 
-
-# import sys
-#
-# from gi.repository import Notify
-# from gi.repository import Gtk
-#
-# if not Notify.init('Notification Test'):
-#     print("ERROR: Could not init Notify.")
-#     sys.exit(1)
-#
-# notification = Notify.Notification.new(
-#     "Notification Title",
-#     "Message...")
-#
-# notification.set_urgency(Notify.Urgency.NORMAL)
-# def actionCallback(notification, action, user_data = None):
-#     print("Callback called:"+action)
-#     Gtk.main_quit()
-#
-# notification.add_action("test-action", "Test Action", actionCallback)
-#
-# if not notification.show():
-#     print("ERROR: Could not show notification.")
-#     sys.exit(2)
-#
-# Gtk.main()
 
 if __name__ == '__main__':
-    main()
+	main()
